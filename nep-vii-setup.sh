994bf3d7580dfb57ec57df48231ecc79ab45dd70
Mitchell Bagot <packof14eggies@gmail.com>
2019-11-01 15:55:41 +1100

    Update nep-vii-setup.sh

--- nep-vii-setup.sh
+++ nep-vii-setup.sh
@@ -3,13 +3,14 @@ OIFS="$IFS"
 IFS=$'\n'
 for file in $(ls -1r ../ | grep Proton)
 do
-  (ln -s "$file/dist/lib/wine/dxvk/*.dll" . && break) || echo "$file directory is empty. Trying another Proton install"
+  (ln -s ../"$file"/dist/lib/wine/dxvk/*.dll . && break) || echo "$file directory is empty. Trying another Proton install"
 done
 IFS="$OIFS"
 # Then, move to the proton prefix
-cd "../compatdata/460120/pfx"
+cd "../../compatdata/460120"
 # Remove and replace with a 32-bit (Since megadimension won't work with 64-bit wine)
-sudo rm -r *
+sudo rm -r pfx
+cd pfx
 WINEARCH=win32 WINEPREFIX=$PWD winetricks devenum quartz wmp10 csmt=on
 cd drive_c/windows
 # Add this directory since it's expected by proton
